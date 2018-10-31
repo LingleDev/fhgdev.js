e246882b041385408b4525adf4d5e52469eaad8b
FHGDev <gideon.lingle1@gmail.com>
2018-10-31 14:02:40 -0700

    Update ws.js

--- src/ws.js
+++ src/ws.js
@@ -8,38 +8,39 @@ const ops = {
   identify: 2,
   incorrect: 4004
 }
+var data = null
+
+const socket = new ws(`wss://gateway.discord.gg/?v=6&encoding=json`)
+
+socket.onopen = () => {
+  console.log("Connected to Discord.")
+}
+
+socket.onmessage = (a) => {
+  const b = JSON.parse(a.data)
+  if (0 == data.op) return;
+  data = b
+}
 
 class WebSocket extends events {
-  gateConnect(token, ready, message) {
-    const socket = new ws(`wss://gateway.discord.gg/?v=6&encoding=json`)
-    socket.onopen = () => {
-      console.log("Connected to Discord.")
-    }
-    socket.onmessage = (a) => {
-      const data = JSON.parse(a.data)
-      if (0 == data.op) return;
-      console.log(data)
-      sequence = data.s
-      10 == b.op
-      && (ws.send(JSON.stringify({
+  gateConnect(token, ready) {
+      ws.send(JSON.stringify({
         op: 2,
         token: token,
         properties: {
           $browser: "Github"
         },
         large_threshold: 50
-      })))
-      
-      if (data.op == ops.incorrect) {
-        throw new RangeError("Incorrect Token.")
-      }
-      
-      if (data.op == ops.ready) {
-        ready.emit("ready")
-      }
-      
-    }
+      }))
   }
-} 
-
-module.exports.Socket = WebSocket
+  
+  if (data.op == ops.incorrect) {
+    throw new RangeError("Incorrect Token.")
+  }
+      
+  if (data.op == ops.ready) {
+    ready.emit("ready")
+  }
+}
+  
+ module.exports.Socket = WebSocket
