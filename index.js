7585047d6359e8a926a0e97f25a7cd1725b3c905
FHGDev <gideon.lingle1@gmail.com>
2018-10-30 17:02:06 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,30 +4,31 @@ const token = 'xd';
 
 client.login(token); 
 
+client.on('ready', () => {
+	// Do something
+})
+
 client.on('message', message => { 
 
-	if(message.content.toLowerCase() === 'help')
-		message.channel.send("good morning " + message.author + "!");
-	else if(message.content.toLowerCase().includes('time'))
+	if (message.content.toLowerCase() === 'help')
+		message.channel.send("good morning " + message.author.username + "!");
+	else if (message.content.toLowerCase().includes('time'))
 	{
 		var date = new Date();
 		message.channel.send('The time is ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
 	}
-	if(message.content.toLowerCase() === 'hello')
-		message.channel.send("hi " + message.author + "(Ahora di retrasado)");
-	else if(message.content.toLowerCase().includes('time'))
+	if (message.content.toLowerCase() === 'hello')
+		message.channel.send("hi " + message.author.username + "(Ahora di retrasado)");
+	else if (message.content.toLowerCase().includes('time'))
 	{
 		var date = new Date();
 		message.channel.send('The time is ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
 	}
-	if(message.content.toLowerCase() === '!waiter')
-	message.channel.send("one second " + message.author + "!");
-else if(message.content.toLowerCase().includes('time'))
-
-{
-	var date = new Date();
-	message.channel.send('The time is ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds());
-}
-
-
+	if (message.content.toLowerCase() === '!waiter')
+	message.channel.send("one second " + message.author.username + "!");
+	
+	const guild = client.guilds.get('id')
+	const channel = guild.channels.find(channel => channel.name === "name")
+	
+	
 });
