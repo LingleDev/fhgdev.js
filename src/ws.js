c0cf4c9975421704cb10aa5463f552e6f2f18504
FHGDev <gideon.lingle1@gmail.com>
2018-11-09 23:22:36 -0500

    Update ws.js

--- src/ws.js
+++ src/ws.js
@@ -13,7 +13,7 @@ socket.onmessage = (a) => {
 }
 
 class WebSocket {
-    this.gateConnect = (token) => {
+    gateConnect = (token) => {
       socket.send(JSON.stringify({
         op: 2,
         d: {
